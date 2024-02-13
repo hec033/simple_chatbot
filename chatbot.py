78b853dcdbabaaf23a7957718441364f0d6758b4
hec033 <heejec@gmail.com>
2024-02-13 10:50:03 -0500

    Working version of Hex

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@ import os
 from openai import OpenAI
 
 
-def chatbot():
+def hex_ai():
     # Load your API key from an environment variable or secret management service
     client = OpenAI(
         # This is the default and can be omitted
@@ -11,7 +11,7 @@ def chatbot():
 
     # create a list to store all messages for context
     messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "system", "content": "You are Hex, an AI assistant. Answer as concisely as possible."},
     ]
 
     # keep repeating the following
@@ -20,26 +20,32 @@ def chatbot():
         message = input("User: ")
 
         # Exit program if user inputs "quit"
-        if message.lower() == "quit":
+        if message.lower() in ["quit", "quit()", "stop", "stop()"]:
             break
 
         # add each message to the list
         messages.append({"role": "user", "content": message})
 
         # request gpt-4-turbo-preview for chat completion
-        completion = client.chat.completions.create(
+        stream_completion = client.chat.completions.create(
             messages=messages,
-            model="gpt-4-turbo-preview"
+            model="gpt-4-turbo-preview",
+            stream=True
         )
 
         # print response and add it to the message list
-        # chat_response = response
-        chat_message = completion.choices[0].message.content
-        print(f"Bot: {chat_message}")
-        messages.append({"role": "assistant", "content": chat_message})
+        print("Hex: ", end="")
+        entire_message = ""
+        for chunk in stream_completion:
+            msg = chunk.choices[0].delta.content or ""
+            print(chunk.choices[0].delta.content or "", end="")
+            entire_message += msg
+
+        # add the bot response to messages
+        messages.append({"role": "assistant", "content": entire_message})
         print()
 
 
 if __name__ == "__main__":
-    print("Starting chatting with the bot (type 'quit' to stop)!")
-    chatbot()
+    print("Starting chatting with Hex (type 'quit' to stop)!")
+    hex_ai()
